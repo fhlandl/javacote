05643667ae71a682a8f74e74a15be1579d249d8e
fhlandl <mkj1206@gmail.com>
2024-05-14 13:06:19 +0900

    init

---
+++ src/main/java/hello/javacote/JavacoteApplication.java
@@ -0,0 +1,13 @@
+package hello.javacote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacoteApplication.class, args);
+	}
+
+}
